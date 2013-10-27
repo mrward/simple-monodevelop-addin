bf5d291808f21e2aad7f03f53d2e72f1356f12d2
Matt Ward <ward.matt@gmail.com>
2013-10-27 20:52:49 +0000

    Create simple MonoDevelop addin.

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Simple MonoDevelop addin
+
+Addin for MonoDevelop 4.0 and Xamarin Studio 4.0 that adds a 
+"Show Simple Addin Message..." menu item to the Project menu. 
+When this menu item is selected a message box will be displayed 
+with the name of the application and the version.
+
---
+++ src/SimpleMonoDevelopAddin/ShowMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+
+namespace SimpleMonoDevelopAddin
+{
+	public class ShowMessageHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			string message = String.Format(
+				"App: {0}, Version: {1}",
+				BrandingService.ApplicationName,
+				IdeApp.Version);
+			MessageService.ShowMessage(message);
+		}
+	}
+}
